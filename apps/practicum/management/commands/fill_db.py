b9f37227e48e6e56f68474f14fad0e9133937a72
bulbazavr <6ulbazavr99@gmail.com>
2024-07-03 19:51:33 +0600

    fix command

--- apps/practicum/management/commands/fill_db.py
+++ apps/practicum/management/commands/fill_db.py
@@ -5,22 +5,21 @@ from apps.practicum.utils import fill_practicum
 
 
 class Command(BaseCommand):
-    help = 'Создает стандартные данные'
+    help = "Создает стандартные данные"
 
     def handle(self, *args, **kwargs):
         fill_practicum()
         fill_academy()
         create_admin()
-        self.stdout.write(
-            self.style.SUCCESS('Стандартные данные успешно созданы')
-        )
+        self.stdout.write(self.style.SUCCESS("Стандартные данные успешно созданы"))
 
 
 from django.contrib.auth.models import User
 
+
 def create_admin():
-    username = 'admin'
-    password = '1'
+    username = "admin"
+    password = "1"
 
     if not User.objects.filter(username=username).exists():
         User.objects.create_superuser(username=username, password=password)
