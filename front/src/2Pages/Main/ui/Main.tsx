e92e89044e5469ee00bd6e825204b1c67d2275fa
Arsen0312 <greenchay16@gmail.com>
2024-07-10 18:15:40 +0600

    fix git bug

--- front/src/2Pages/Main/ui/Main.tsx
+++ front/src/2Pages/Main/ui/Main.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import cls from "./Main.module.scss"
 import WelcomeToPracticum from "./WelcomeToPracticum/ui/WelcomeToPracticum";
+import PopularCourses from "./PopularCourses/ui/PopularCourses";
 
 
 const Main = () => {
@@ -8,6 +9,7 @@ const Main = () => {
     return (
         <div className={cls.main}>
             <WelcomeToPracticum/>
+            <PopularCourses/>
         </div>
     );
 };
--- front/src/2Pages/Main/ui/WelcomeToPracticum/ui/WelcomeToPracticum.tsx
+++ front/src/2Pages/Main/ui/WelcomeToPracticum/ui/WelcomeToPracticum.tsx
@@ -1,14 +1,33 @@
 import React from 'react';
 import cls from "./WelcomeToPracticum.module.scss"
+import {Link} from "react-router-dom";
 
 const WelcomeToPracticum = () => {
 
     return (
         <div className={cls.main}>
             <div className={cls.overlay}></div>
+            <div className={cls.wrapperContent}>
                 <h1>
                     Добро пожаловать в Практикум
                 </h1>
+                <p>
+                    Наши услуги
+                </p>
+                <div className={cls.wrapperNavigation}>
+                    <Link to={''}>
+                        Аутсорсинг бухгалтерии
+                    </Link>
+                    <Link to={''}>
+                        Бухгалтерия для начинающих
+                    </Link>
+                    <Link to={''}>
+                        1C предприятие
+                    </Link>
+                    <Link to={''}>
+                        Подготовка бизнеса
+                    </Link>
+                </div>
             </div>
         </div>
     );
