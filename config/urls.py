97f7575940c1fea49eb675cfd2dc6b645d3ae937
Arsen0312 <greenchay16@gmail.com>
2024-07-11 16:58:50 +0600

    update master branch

--- config/urls.py
+++ config/urls.py
@@ -3,7 +3,13 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView, SpectacularRedocView
 
+swagger_patterns = [
+    path('', SpectacularAPIView.as_view(), name='schema'),
+    path('swagger-ui/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
+    path('redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
+]
 
 api_patterns = [
     path("academy/", include("apps.academy.urls")),
@@ -11,7 +17,14 @@ api_patterns = [
     path("account/", include("apps.account.urls")),
     path("outsourcing/", include("apps.outsourcing.urls")),
     path("admin/", admin.site.urls),
+    path('schema/', include(swagger_patterns))
+
+]
+
+
+urlpatterns = [
     path("api/v1/", include(api_patterns)),
+]
 
 
 if settings.DEBUG:
