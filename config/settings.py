f822e48e5efc8c4a7346ab1d1494bf3302541100
Dastan Oskonbaev <dastiw1910@gmail.com>
2024-07-15 16:38:06 +0600

    Cors

--- config/settings.py
+++ config/settings.py
@@ -47,6 +47,8 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
 ]
 
 
@@ -120,6 +122,23 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 CORS_ALLOW_CREDENTIALS = config("CORS_ALLOW_CREDENTIALS", cast=bool)
 CORS_ALLOWED_ORIGINS = config("CORS_ALLOWED_ORIGINS").split(", ")
+CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOWED_METHODS = [
+    'GET',
+    'OPTIONS',
+    'POST',
+]
+CORS_ALLOWED_HEADERS = [
+    'accept',
+    'accept-encoding',
+    'authorization',
+    'content-type',
+    'dnt',
+    'origin',
+    'user-agent',
+    'x-csrftoken',
+    'x-requested-with',
+]
 
 
 X_FRAME_OPTIONS = "SAMEORIGIN"
